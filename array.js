9dd6f74a7cb372df3f611f34e5856fa11be799e6
David Felix <davidffelix@hotmail.com>
2022-02-04 10:19:11 -0300

    Array

---
+++ array.js
@@ -0,0 +1,8 @@
+let num = [5, 8, 4];
+num[3] = 9;
+num.sort();
+num.push(7);
+
+console.log(num);
+console.log(`The length of the array is ${num.length}.`);
+console.log(`The position of the third element is ${num[3]}.`);
